579580d35602e1cb4f12fd36b894e9636f88df14
ZimaBlue <1216912253@qq.com>
2021-12-22 23:16:32 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/dog.gif">
+    <HelloWorld msg="都是兄弟，别客气"/>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,90 @@
-<template>
+<template id="app">
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <el-input style="width: 70%" clearable placeholder="请输入内容" v-model="keyWord" class="input-with-select"
+              @change="onSearch">
+      <el-button slot="append" icon="el-icon-search" @click="searchVideo">搜索</el-button>
+    </el-input>
+
+    <template>
+      <el-table
+          v-show="flag"
+          v-loading="loading"
+          element-loading-text="你健正在拼了命的搜索中"
+          element-loading-spinner="el-icon-loading"
+          element-loading-background="rgba(0, 0, 0, 0.8)"
+          :data="tableData"
+          stripe
+          show-overflow-tooltip
+          highlight-current-row
+          empty-text="暂无数据"
+          align="center"
+          style="width: 100%">
+        <el-table-column
+            prop="question"
+            align="center"
+            label="资源">
+        </el-table-column>
+        <el-table-column
+            prop="answer"
+            align="center"
+            label="链接">
+        </el-table-column>
+      </el-table>
+    </template>
+
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import {Table, TableColumn, Loading, Input, Icon, Button} from 'element-ui';
+import {queryFilms} from "../utils/api";
+
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Loading)
+Vue.use(Input)
+Vue.use(Icon)
+Vue.use(Button)
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data() {
+    return {
+      keyWord: '',
+      list: [],
+      loading: false,
+      tableData: [],
+      flag: false,
+    };
+  },
+  methods: {
+    onSearch() {
+      this.searchVideo()
+    },
+    searchVideo() {
+      this.flag = true
+      this.loading = true;
+
+      queryFilms(this.keyWord).then(res => {
+        this.tableData = res.data.list
+        this.loading = false;
+      }).catch(err => {
+        console.log(err);
+        this.tableData = [{"question": "服务器居然出故障了!", "answer": "请稍后再试或者联系你健噢"}]
+        this.loading = false;
+      });
+
+    },
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+<style>
+
 </style>

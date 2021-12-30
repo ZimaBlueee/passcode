08c1dec68e3f9b0366d3b8404cba11d046b59bcb
ZimaBlue <1216912253@qq.com>
2021-12-30 20:14:26 +0800

    codepen test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,12 +3,17 @@
     <el-container>
       <el-header height="50px">
         <img src="../../static/img/naleme.png" alt="" class="logo1" @click="toRouter('/home')">
-        <div class="navbar">
-          <span @click="toRouter('/home')">主页</span>
-          <span @click="toRouter('/movie')">影视搜索</span>
-          <span @click="toRouter('/calculator')">身高计算器</span>
-          <span @click="notify">程序员类文档阅读</span>
-          <span @click="notify">程序员类视频观看</span>
+        <div class="navbar frame">
+          <button class="custom-btn btn-5" @click="toRouter('/home')"><span>主页</span></button>
+          <button class="custom-btn btn-12" @click="toRouter('/movie')"><span>你想要的这里都有</span><span>影视搜索</span></button>
+          <button class="custom-btn btn-8" @click="toRouter('/calculator')"><span>身高计算器</span></button>
+          <button class="custom-btn btn-14" @click="notify">文档阅读</button>
+          <button class="custom-btn btn-13" @click="notify">视频观看</button>
+          <!--<span @click="toRouter('/home')">主页</span>-->
+          <!--<span @click="toRouter('/movie')">影视搜索</span>-->
+          <!--<span @click="toRouter('/calculator')">身高计算器</span>-->
+          <!--<span @click="notify">程序员类文档阅读</span>-->
+          <!--<span @click="notify">程序员类视频观看</span>-->
         </div>
         <div class="clean"></div>
       </el-header>
@@ -33,7 +38,7 @@ export default {
 
       this.$notify({
         title: 'Tips',
-        message: h('i', { style: 'color: teal'}, '正在开发中，敬请期待哦')
+        message: h('i', {style: 'color: teal'}, '正在开发中，敬请期待哦')
       });
     },
   },
@@ -45,21 +50,22 @@ export default {
 
 <style lang="scss">
 .el-header {
-  .navbar{
+  .navbar {
     text-align: right;
-    span {
-      display: inline-block;
-      font-size: .3rem;
-      line-height: .50rem;
-      color: #222222;
-      margin:0 10px ;
-      cursor: pointer;
-    }
+
+    //span {
+    //  display: inline-block;
+    //  font-size: .3rem;
+    //  line-height: .50rem;
+    //  color: #222222;
+    //  margin: 0 10px;
+    //  cursor: pointer;
+    //}
   }
 
 }
 
-.logo1{
+.logo1 {
   float: left;
 }
 

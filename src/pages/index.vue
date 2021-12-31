d315c940095232185eb9be6ee882e54230b87232
ZimaBlue <1216912253@qq.com>
2021-12-31 17:34:16 +0800

    适配移动端

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,16 +4,22 @@
       <el-header height="50px">
         <img src="../../static/img/naleme.png" alt="" class="logo1" @click="toRouter('/home')">
         <div class="navbar">
-          <button class="custom-btn btn-6" @click="toRouter('/home')"><span>主页</span></button>
-          <button class="custom-btn btn-12" @click="toRouter('/movie')"><span>你想要的这里都有</span><span>影视搜索</span></button>
-          <button class="custom-btn btn-8" @click="toRouter('/calculator')"><span>身高计算器</span></button>
-          <button class="custom-btn btn-14" @click="notify">文档阅读</button>
-          <button class="custom-btn btn-13" @click="notify">视频观看</button>
-          <!--<span @click="toRouter('/home')">主页</span>-->
-          <!--<span @click="toRouter('/movie')">影视搜索</span>-->
-          <!--<span @click="toRouter('/calculator')">身高计算器</span>-->
-          <!--<span @click="notify">程序员类文档阅读</span>-->
-          <!--<span @click="notify">程序员类视频观看</span>-->
+          <div v-if=flag>
+            <button class="custom-btn btn-6" @click="toRouter('/home')"><span>主页</span></button>
+            <button class="custom-btn btn-12" @click="toRouter('/movie')"><span>你想要的这里都有</span><span>影视搜索</span>
+            </button>
+            <button class="custom-btn btn-8" @click="toRouter('/calculator')"><span>身高计算器</span></button>
+            <button class="custom-btn btn-14" @click="notify">文档阅读</button>
+            <button class="custom-btn btn-13" @click="notify">视频观看</button>
+          </div>
+
+          <div v-if=!flag>
+            <span1 @click="toRouter('/home')">主页</span1>
+            <span1 @click="toRouter('/movie')">影视搜索</span1>
+            <span1 @click="toRouter('/calculator')">身高计算器</span1>
+            <span1 @click="notify">程序员类文档阅读</span1>
+            <span1 @click="notify">程序员类视频观看</span1>
+          </div>
         </div>
         <div class="clean"></div>
       </el-header>
@@ -27,7 +33,9 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      flag: true
+    }
   },
   methods: {
     toRouter(val) {
@@ -43,6 +51,15 @@ export default {
     },
   },
   mounted() {
+  },
+  created() {
+    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+      /*window.location.href="你的手机版地址";*/
+      this.flag = false;
+    } else {
+      /*window.location.href="你的电脑版地址";    */
+      this.flag = true;
+    }
   }
 
 }
@@ -53,14 +70,14 @@ export default {
   .navbar {
     text-align: right;
 
-    //span {
-    //  display: inline-block;
-    //  font-size: .3rem;
-    //  line-height: .50rem;
-    //  color: #222222;
-    //  margin: 0 10px;
-    //  cursor: pointer;
-    //}
+    span1 {
+      display: inline-block;
+      font-size: .3rem;
+      line-height: .50rem;
+      color: #222222;
+      margin: 0 10px;
+      cursor: pointer;
+    }
   }
 
 }

eac09c5b06c48eac285049f9b2af984fad2b0a73
ZimaBlue <1216912253@qq.com>
2022-05-07 00:05:55 +0800

    增加提示信息

--- naleme-f-release.sh
+++ naleme-f-release.sh
@@ -6,15 +6,18 @@ cd /home/naleme/film-search || exit 1
 
 #git checkout master
 #git checkout .
+echo '==============开始拉取最新代码==========='
 git pull --rebase
-echo '==============git pull success=========='
+echo '==============拉取最新代码成功=========='
 
 sleep 1
 
+echo '==============开始打包镜像================'
 docker-compose build
 echo '==============镜像打包成功================'
 
+echo '==============开始部署容器================'
 docker-compose up -d
 echo '==============容器部署成功================'
 
-echo '==============run success================'
+echo '==============前端发布成功!================'

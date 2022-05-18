7e491e2104f0dfc8661038700a6ba6cd34b3df98
ZimaBlue <1216912253@qq.com>
2022-05-18 09:50:05 +0800

    食堂访客

--- src/pages/applyCanteenVisitor.vue
+++ src/pages/applyCanteenVisitor.vue
@@ -41,15 +41,63 @@ export default {
       this.$router.push(val);
     },
 
+    // onSubmit() {
+    //   this.axios({
+    //     method: 'get',
+    //     url: '/meal/generateVisitorQRCode?phone=' + this.form.phone,
+    //     responseType: 'blob'
+    //   }).then(res => {
+    //     console.log("返回值=" + res)
+    //     this.verifycode = window.URL.createObjectURL(res)
+    //   })
+    // },
+
     onSubmit() {
+      let flag1 = true
+      let that = this
       this.axios({
         method: 'get',
         url: '/meal/generateVisitorQRCode?phone=' + this.form.phone,
         responseType: 'blob'
       }).then(res => {
-        console.log("返回值="+res)
-        this.verifycode = window.URL.createObjectURL(res)
-      })
+          let data = res;
+          let fileReader = new FileReader();
+          fileReader.onload = function () {
+            try {
+              let jsonData = JSON.parse(this.result);
+              if (jsonData.code) {
+                flag1 = false
+                console.log(flag1)
+                // 报错
+                that.$message({
+                  showClose: true,
+                  message: jsonData.msg,
+                  type: 'error'
+                });
+              }
+            } catch (err) {
+              // 解析成对象失败，说明是正常的文件流
+              that.verifycode = window.URL.createObjectURL(data)
+            }
+          };
+          fileReader.readAsText(data);
+
+          console.log(flag1)
+          if (flag1) {
+            // 检查次数
+            this.axios
+              .get('/meal/checkCount?phone=' + this.form.phone)
+              .then(res => {
+                console.log(res)
+
+                this.$alert(res.data, '员工须知', {
+                  dangerouslyUseHTMLString: true
+                });
+              });
+          }
+        }
+      )
+
     },
 
     querySearch(queryString, cb) {
@@ -76,15 +124,5 @@ export default {
 </script>
 
 <style lang="scss">
-.el-autocomplete {
-  width: 8rem;
-}
-
-//label{
-//  display:inline-block;
-//  width:2rem;
-//
-//  text-align:right;
-//}
 
 </style>
